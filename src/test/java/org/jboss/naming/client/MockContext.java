29220b657bd250b89dc2bf82ea26754abcb7047b
John E. Bailey <jbailey@redhat.com>
2012-01-12 10:52:09 -0600

    Some cleanup

--- src/test/java/org/jboss/naming/client/MockContext.java
+++ src/test/java/org/jboss/naming/client/MockContext.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.naming.client;
 
 import java.util.ArrayList;
@@ -15,9 +36,7 @@ import javax.naming.NameNotFoundException;
 import javax.naming.NameParser;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
-import javax.naming.spi.NamingManager;
 import static org.jboss.naming.client.ClientUtil.namingEnumeration;
-import static org.jboss.naming.client.ClientUtil.namingException;
 
 /**
  * @author John Bailey
